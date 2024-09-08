2b7d6af620da17f90e91819df265b942ad3fe9ed
pawan-live <pawan.senpura@gmail.com>
2024-09-08 07:19:00 +0530

    fix: update vite config

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,6 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const isProduction = mode === "production";
-  const base = isProduction ? "/sliitxtreme-web-2024/" : "/";
-
-  return {
-    base,
-    plugins: [react()],
-  };
+export default defineConfig({
+  plugins: [react()],
 });
