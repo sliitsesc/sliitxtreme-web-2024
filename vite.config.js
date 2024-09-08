1594d8314aecfa855337cd1525111b35a5ac3ebd
pawan-live <pawan.senpura@gmail.com>
2024-09-08 06:18:21 +0530

    build: add environment based base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === "production";
+  const base = isProduction ? "/sliitxtreme-web-2024/" : "/";
+
+  return {
+    base,
+    plugins: [react()],
+  };
 });
