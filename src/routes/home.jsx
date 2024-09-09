ba9289e51bf8c87600f4d3d54f4f32ed5b270015
SadeeshaPerera <121984253+SadeeshaPerera@users.noreply.github.com>
2024-09-10 03:01:33 +0530

    feat: Add Timeline component to Home page

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,7 +1,8 @@
-import Hero from "../components/Hero/Hero";
-import InfoSection from "../components/Info/InfoSection";
 import Awards from "../components/Awards/Awards";
 import FAQ from "../components/FAQ/FAQ";
+import Hero from "../components/Hero/Hero";
+import InfoSection from "../components/Info/InfoSection";
+import Timeline from "../components/Timeline/timeline";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero />
       <InfoSection />
       <Awards />
+      <Timeline />
       <FAQ />
     </>
   );
