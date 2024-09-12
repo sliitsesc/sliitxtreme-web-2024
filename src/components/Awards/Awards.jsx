46142f873c6bb047d0cd5fb58a4aab2567579b47
SadeeshaPerera <121984253+SadeeshaPerera@users.noreply.github.com>
2024-09-12 19:31:54 +0530

    chore: Update award section

--- src/components/Awards/Awards.jsx
+++ src/components/Awards/Awards.jsx
@@ -1,44 +1,65 @@
-import { FaTrophy, FaMedal, FaAward } from 'react-icons/fa';
-
 const Awards = () => {
   return (
     <section className="py-12">
-      <div className="container bg-gray-900 mx-auto px-8 py-10 text-center rounded-md">
-        <h2 className="text-5xl font-bold mb-12 color-white text-white">Awards</h2>
+      <div className="container bg-white mx-auto px-8 py-10 text-center rounded-md">
+        <div className="text-center mb-8">
+          <h1 className="text-4xl font-bold text-gray-800 mb-8">Awards</h1>
 
+          <p className="text-lg text-gray-600">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
         {/* cards container */}
-        <div className="flex flex-col sm:flex-row justify-center gap-y-8 sm:gap-x-12 p-6">
-
+        <div className="flex flex-col sm:flex-row justify-center gap-y-8 sm:gap-x-2 p-6">
           {/* 1st Runner up */}
-          <div className="w-full sm:w-[30%] bg-white border border-gray-200 rounded-lg shadow-lg transition-transform transform hover:scale-105 hover:shadow-xl flex flex-col justify-center items-center py-10 gap-y-6">
-            <FaMedal className="text-red-500 w-[100px] h-[100px]" /> {/* Medal Icon */}
+          <div className="w-full sm:w-[30%] bg-white border border-white rounded-lg  transition-transform transform flex flex-col justify-center items-center py-10 gap-y-6 order-2 sm:order-none">
+            <img
+              src="../src/assets/images/awards/award_2.png"
+              alt="Description of image"
+              className="w-[125px] h-[125px]  object-contain"
+            />
+            {/* Medal Icon */}
             <div className="text-center">
-              <h2 className="text-3xl font-bold text-gray-800">1st Runner up</h2>
+              <h2 className="text-3xl font-bold text-gray-800">
+                1st Runner up
+              </h2>
               <h3 className="text-xl font-semibold text-gray-600 mt-2">
-                40,000 LKR
+                25,000 LKR
               </h3>
             </div>
           </div>
 
           {/* Champion card */}
-          <div className="w-full sm:w-[30%] bg-white border border-gray-200 rounded-lg shadow-lg transition-transform transform hover:scale-115 hover:shadow-xl flex flex-col justify-center items-center py-10 gap-y-6 scale-110 ">
-
-            <FaTrophy className="text-yellow-500 w-[100px] h-[100px]" /> {/* Trophy Icon */}
+          <div className="w-full sm:w-[30%] h-[400px] bg-white border border-white rounded-lg  transition-transform transform  flex flex-col justify-center items-center py-10 gap-y-6 scale-110 order-1 sm:order-none">
+            <img
+              src="../src/assets/images/awards/award_1.png"
+              alt="Description of image"
+              className="w-[150px] h-[150px]  object-contain"
+            />
+            {/* Trophy Icon */}
             <div className="text-center">
-              <h2 className="text-3xl font-bold text-gray-800">Champion</h2>
+              <h2 className="text-3xl font-bold text-gray-800">Winner</h2>
               <h3 className="text-xl font-semibold text-gray-600 mt-2">
-                30,000 LKR
+                50,000 LKR
               </h3>
             </div>
           </div>
 
           {/* 2nd Runner up card */}
-          <div className="w-full sm:w-[30%] bg-white border border-gray-200 rounded-lg shadow-lg transition-transform transform hover:scale-105 hover:shadow-xl flex flex-col justify-center items-center py-10 gap-y-6">
-            <FaAward className="text-green-500 w-[100px] h-[100px]" /> {/* Award Icon */}
+          <div className="w-full sm:w-[30%] bg-white border border-white rounded-lg  transition-transform transform  flex flex-col justify-center items-center py-10 gap-y-6 order-3 sm:order-none">
+            <img
+              src="../src/assets/images/awards/award_3.png"
+              alt="Description of image"
+              className="w-[125px] h-[125px]  object-contain"
+            />
+            {/* Award Icon */}
             <div className="text-center">
-              <h2 className="text-3xl font-bold text-gray-800">2nd Runner up</h2>
+              <h2 className="text-3xl font-bold text-gray-800">
+                2nd Runner up
+              </h2>
               <h3 className="text-xl font-semibold text-gray-600 mt-2">
-                20,000 LKR
+                10,000 LKR
               </h3>
             </div>
           </div>
