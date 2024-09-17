495cddb78cdf0ec50e7af979e5eac2bd15227c17
pawan-live <pawan.senpura@gmail.com>
2024-09-17 14:10:41 +0530

    refactor: remove old components from view, updated several styles

--- src/components/Info/InfoSection.jsx
+++ src/components/Info/InfoSection.jsx
@@ -1,61 +1,47 @@
-import { Swiper, SwiperSlide } from "swiper/react";
+import parse from "html-react-parser";
 import "swiper/css";
-import "swiper/css/pagination";
 import "swiper/css/autoplay";
-import { Pagination, Autoplay } from "swiper/modules";
+import "swiper/css/pagination";
+import { Autoplay, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { landingPageData } from "../../lib/data/landingPageData";
 
 export default function InfoSection() {
   return (
     <section>
-      <div className="container mx-auto flex flex-col md:flex-row gap-y-10 md:gap-x-10 py-16 px-8">
+      <div className="container mx-auto flex flex-col lg:flex-row gap-y-10 md:gap-x-10 pt-10 pb-20 px-8">
         {/* Image Slider */}
         <div className="w-full bg-slate-200 overflow-hidden rounded-3xl">
           <Swiper
-            spaceBetween={10}
+            spaceBetween={0}
             slidesPerView={1}
             loop={true}
             autoplay={{ delay: 3000 }}
             pagination={{ clickable: true }}
             modules={[Pagination, Autoplay]}
           >
-            <SwiperSlide>
-              <div className="relative w-full h-[400px] overflow-hidden ">
-                <img
-                  src="https://i.pinimg.com/1200x/91/92/c9/9192c99c14e8f9d303a5ecfefd96ecc9.jpg"
-                  className="w-full h-full object-cover grayscale"
-                  alt="Demo Image"
-                />
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="relative w-full h-[400px] overflow-hidden">
-                <img
-                  src="https://assets.website-files.com/5b3dd54182ecae4d1602962f/609e33e18c5000af6211f094_HR%20Hackathon%20-%20Section%202.jpg"
-                  className="w-full h-full object-cover grayscale"
-                  alt="Image 2"
-                />
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="relative w-full h-[400px] overflow-hidden">
-                <img
-                  src="https://miro.medium.com/v2/resize:fit:1400/1*E-MJcNbKGHdrcCKUU6r9xg.jpeg"
-                  className="w-full h-full object-cover grayscale"
-                  alt="Image 3"
-                />
-              </div>
-            </SwiperSlide>
-            {/* Add more SwiperSlide components as needed */}
+            {landingPageData?.aboutSection?.images.map((image, index) => (
+              <SwiperSlide key={index}>
+                <div className="relative w-full max-h-[450px] overflow-hidden ">
+                  <img
+                    src={image.src}
+                    className="w-full h-full object-cover grayscale"
+                    alt={image.alt}
+                  />
+                </div>
+              </SwiperSlide>
+            ))}
           </Swiper>
         </div>
 
         {/* Content */}
-        <div className="w-full flex flex-col justify-center">
-          <h1 className="text-5xl font-bold">What is SLIITXtreme?</h1>
+        <div className="w-full flex flex-col justify-center text-white dm-sans-400">
+          <h1 className="text-[48px] font-bold drop-shadow-white-glow">
+            {landingPageData?.aboutSection?.title}
+          </h1>
           <p className="py-6 text-xl">
-            The IEEE Computer Society of Sri Lanka Institute of Information Technology (SLIIT) organizes “SLIITXtreme 3.0” in collaboration with the Software Engineering Student Community of SLIIT,
-            with the aim of conducting an Exclusive Programming Competition for SLIIT Students.
-            This competition aims to introduce students of SLIIT to the IEEEXtreme global programming competition.
+            {parse(landingPageData?.aboutSection?.content)}
           </p>
         </div>
       </div>
--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,23 +1,11 @@
-import Awards from "../components/Awards/Awards";
-import FAQ from "../components/FAQ/FAQ";
-import Guidelines from "../components/Guidelines/Guidelines";
 import Hero from "../components/Hero/Hero";
 import InfoSection from "../components/Info/InfoSection";
-import Stats from "../components/Stats/Stats";
-import Testimonial from "../components/Testimonial/Testimonial";
-import Timeline from "../components/Timeline/Timeline";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <InfoSection />
-      <Awards />
-      <Testimonial />
-      <Guidelines />
-      <Timeline />
-      <FAQ />
-      <Stats />
     </>
   );
 }
